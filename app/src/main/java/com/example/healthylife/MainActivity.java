409b0af8b90b9ca0b191d6352f4f3fcaa82ec621
RostyslavNovoselskyi <62751385+RostyslavNovoselskyi@users.noreply.github.com>
2020-05-09 19:42:07 +0300

    Added Register and sign in window with db users

--- app/src/main/java/com/example/healthylife/MainActivity.java
+++ app/src/main/java/com/example/healthylife/MainActivity.java
@@ -1,55 +1,213 @@
 package com.example.healthylife;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 
+import com.example.healthylife.Models.User;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.rengwuxian.materialedittext.MaterialEditText;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.StringRes;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.text.TextUtils;
+import android.util.Patterns;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
+
+import java.util.regex.Pattern;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+    Button btnSignIn, btnSignUp;
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+    FirebaseAuth auth;
+    FirebaseDatabase db;
+    DatabaseReference users;
+
+    RelativeLayout root;
+
+    private void showMessage(@StringRes int string){
+        Toast.makeText(this, string, Toast.LENGTH_LONG).show();
+    }
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_first);
+
+        btnSignIn = findViewById(R.id.btnSignIn);
+        btnSignUp = findViewById(R.id.btnSignUp);
+
+        root = findViewById(R.id.root_element);
+
+        auth = FirebaseAuth.getInstance();
+        db = FirebaseDatabase.getInstance();
+        users = db.getReference("Users");
+
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onClick(View v) {
+                showRegisterWindow();
             }
+
+        });
+
+        btnSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showSignInWindow();
+            }
+
         });
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+    private void showSignInWindow(){
+        AlertDialog.Builder dialog = new AlertDialog.Builder(this);
+        dialog.setTitle("Log in");
+        dialog.setMessage("Enter login information");
+
+        LayoutInflater inflater = LayoutInflater.from(this);
+        View signInWindow = inflater.inflate(R.layout.sign_in_window, null);
+        dialog.setView(signInWindow);
+
+        final MaterialEditText email = signInWindow.findViewById(R.id.emailField);
+        final MaterialEditText password = signInWindow.findViewById(R.id.passField);
+
+
+        dialog.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int which) {
+                dialogInterface.dismiss();
+            }
+        });
+        dialog.setPositiveButton("LogIn", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int which) {
+                if (TextUtils.isEmpty(email.getText().toString())){
+                    Snackbar.make(root, "Enter your email", Snackbar.LENGTH_SHORT).show();
+                    return;
+                }
+                if(!Patterns.EMAIL_ADDRESS.matcher(email.getText()).matches()){
+                    showMessage(R.string.email_input_error);
+                }
+
+                if (password.getText().toString().length() < 5){
+                    Snackbar.make(root, "Numerous of password must be more than 5 symbols", Snackbar.LENGTH_SHORT).show();
+                    return;
+                }
+
+                auth.signInWithEmailAndPassword(email.getText().toString(), password.getText().toString())
+                .addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                    @Override
+                    public void onSuccess(AuthResult authResult) {
+                        startActivity(new Intent(MainActivity.this, SecondActivity.class));
+                        finish();
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Snackbar.make(root, "LogIn Error. " + e.getMessage(), Snackbar.LENGTH_SHORT).show();
+                    }
+                });
+            }
+        });
+
+        dialog.show();
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
+    private void showRegisterWindow() {
+        AlertDialog.Builder dialog = new AlertDialog.Builder(this);
+        dialog.setTitle("Register");
+        dialog.setMessage("Enter all data for registration");
 
-        return super.onOptionsItemSelected(item);
+        LayoutInflater inflater = LayoutInflater.from(this);
+        View registerWindow = inflater.inflate(R.layout.register_window, null);
+        dialog.setView(registerWindow);
+
+        final MaterialEditText email = registerWindow.findViewById(R.id.emailField);
+        final MaterialEditText password = registerWindow.findViewById(R.id.passField);
+        final MaterialEditText name = registerWindow.findViewById(R.id.nameField);
+        final MaterialEditText phone = registerWindow.findViewById(R.id.phoneField);
+
+        dialog.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int which) {
+                dialogInterface.dismiss();
+            }
+        });
+        dialog.setPositiveButton("Submit", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int which) {
+                if (TextUtils.isEmpty(email.getText().toString())){
+                    Snackbar.make(root, "Enter your email", Snackbar.LENGTH_SHORT).show();
+                    return;
+                }
+                if(!Patterns.EMAIL_ADDRESS.matcher(email.getText()).matches()){
+                    showMessage(R.string.email_input_error);
+                }
+
+                if (TextUtils.isEmpty(name.getText().toString())){
+                    Snackbar.make(root, "Enter your name", Snackbar.LENGTH_SHORT).show();
+                    return;
+                }
+                if (TextUtils.isEmpty(phone.getText().toString())){
+                    Snackbar.make(root, "Enter your phone", Snackbar.LENGTH_SHORT).show();
+                    return;
+                }
+                if (password.getText().toString().length() < 5){
+                    Snackbar.make(root, "Numerous of password must be more than 5 symbols", Snackbar.LENGTH_SHORT).show();
+                    return;
+                }
+
+                //Registration
+                auth.createUserWithEmailAndPassword(email.getText().toString(), password.getText().toString())
+                        .addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                            @Override
+                            public void onSuccess(AuthResult authResult) {
+                                User user = new User();
+                                user.setEmail(email.getText().toString());
+                                user.setName(name.getText().toString());
+                                user.setPhone(phone.getText().toString());
+                                user.setPass(password.getText().toString());
+
+                                users.child(FirebaseAuth.getInstance().getCurrentUser().getUid())
+                                        .setValue(user)
+                                        .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                            @Override
+                                            public void onSuccess(Void aVoid) {
+                                                Snackbar.make(root, "User added!", Snackbar.LENGTH_SHORT).show();
+                                            }
+                                        });
+                            }
+                        }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Snackbar.make(root, "Registration error. "+ e.getMessage(), Snackbar.LENGTH_LONG).show();
+                    }
+                });
+            }
+        });
+
+        dialog.show();
     }
+
 }
